79a47cbeb61e1e1e7199c977117f5a0b2b3b49c6
Taku1220 <152232473+Taku20200824@users.noreply.github.com>
2026-08-12 10:36:30 +0900

    Update default server status fallback

--- src/lib/firebase/server-status-shared.ts
+++ src/lib/firebase/server-status-shared.ts
@@ -28,13 +28,13 @@ export type ServerStatusDocument = z.infer<typeof serverStatusSchema> & {
 };
 
 export const initialServerStatus = {
-  serverName: "TAKU's The Isle",
+  serverName: "ASIA JP,MNG,KR Test",
   status: "online",
-  ip: "",
-  port: 7777,
-  location: "Hong Kong",
+  ip: "209.102.250.73",
+  port: 9075,
+  location: "Singapore",
   onlinePlayers: 0,
-  maxPlayers: 100,
+  maxPlayers: 32,
   version: "Evrima",
   map: "Gateway",
   discordUrl: "https://discord.gg/vmn3YjCZSE",
